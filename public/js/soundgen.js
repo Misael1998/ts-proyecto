8a2cb7f6a802949d169b11d6e3eaf06ab790c2b8
Misael <misalande@gmail.com>
2020-08-18 02:17:29 -0600

    cargando cancion, falta generar onda visual

--- public/js/soundgen.js
+++ public/js/soundgen.js
@@ -4,31 +4,51 @@ const song = document.getElementById("file");
 let songCanvas;
 let loadedSong;
 
-let songGraphSketch = (obj) => {
-  return (p) => {
-    p.preload = () => {
-      loadedSong = p.loadSound(obj);
-    };
+let songGraphSketch = (p) => {
+  let urlObj;
+  let fft;
 
-    p.setup = () => {
-      p.createCanvas(500, 250);
+  p.preload = () => {
+    try {
+      urlObj = URL.createObjectURL(song.files[0]);
+      loadedSong = p.loadSound(urlObj);
+    } catch (err) {}
+  };
+
+  p.setup = () => {
+    p.createCanvas(500, 250);
+    if (loadedSong) {
       loadedSong.setVolume(1);
-    };
+      fft = new p5.FFT();
+    }
+  };
 
-    p.draw = () => {
-      p.background(0);
-    };
+  p.draw = () => {
+    p.background(255);
+    if (loadedSong) {
+      let spectrum = fft.waveform();
+      p.noStroke();
+
+      p.noFill();
+      p.beginShape();
+      p.stroke(20);
+      for (let i = 0; i < spectrum.length; i++) {
+        let amp = spectrum[i];
+        let x = p.map(i, 0, spectrum.length, p.width);
+        let y = p.map(amp, -1, 1, 0, p.height);
+        p.vertex(x, y);
+      }
+      p.endShape();
+    }
   };
 };
 
+let soundGraphObj = new p5(songGraphSketch, songGraphCanvas);
+
 const onSubmit = (e) => {
   e.preventDefault();
-  console.log(song.files);
-  if (!song.files) return;
-
-  const urlObj = URL.createObjectURL(song.files[0]);
-  let graph = songGraphSketch(urlObj);
-  const soundGraphObj = new p5(graph, songGraphCanvas);
+  soundGraphObj.preload();
+  soundGraphObj.setup();
 };
 
 const onPlayPause = (e) => {
@@ -37,6 +57,7 @@ const onPlayPause = (e) => {
     loadedSong.pause();
   } else {
     loadedSong.play();
+    soundGraphObj.draw();
   }
 };
 
